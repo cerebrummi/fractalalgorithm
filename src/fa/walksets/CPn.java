d2dd09e9f5247489f37dac1f7ca344b2b6b428e5
birkeheeren <kontakt@dornrose.art>
2025-08-14 20:56:49 +0200

    Revert "implement sieves"
    This reverts commit 9dc90fcb37552ecf008fa9a2c581523a17c01b6d.

--- src/fa/walksets/CPn.java
+++ src/fa/walksets/CPn.java
@@ -63,14 +63,4 @@ public class CPn implements Walkset
    {
       return "CPn = < " + Arrays.toString(list.toArray()) + " >";
    }
-   
-   public int getCurrentSize()
-   {
-      return list.size();
-   }
-
-   public LinkedList<Symbol> getList()
-   {
-      return list;
-   }
 }
